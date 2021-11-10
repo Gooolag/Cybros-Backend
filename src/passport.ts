1f47e6ab204f1b26833035cabcc2295aad531852
shivral somani <shivral312002@gmail.com>
2021-11-10 16:43:14 +0530

    res

---
+++ src/passport.ts
@@ -0,0 +1,22 @@
+import passport  from "passport";
+import { Strategy } from "passport-google-oauth2";
+
+// passport.serializeUser(function(user, done) {
+//     done(null, user);
+// });
+
+// passport.deserializeUser(function(user, done) {
+//         done(null, user);
+// });
+
+var GoogleStrategy=Strategy;
+passport.use(new GoogleStrategy({
+      clientID:"527085962224-ll9s87m90fon1jl6c4ef6dadp52pcc7m.apps.googleusercontent.com",
+    clientSecret:"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",
+    callbackURL: "http://localhost:3000/auth/google/callback",
+    passReqToCallback   : true
+},
+function(request:any, accessToken:any, refreshToken:any, profile:any, done:any) {
+    console.log("hje");
+  }
+))
