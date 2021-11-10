1d8c22fa8bcaf957d6fdcb8dc848dc2cda1012d4
dreadarceus <dreadarceus@gmail.com>
2021-11-10 18:09:49 +0530

    pls work 2

--- src/passport.ts
+++ src/passport.ts
@@ -13,7 +13,7 @@ var GoogleStrategy=Strategy;
 passport.use(new GoogleStrategy({
       clientID:"527085962224-ll9s87m90fon1jl6c4ef6dadp52pcc7m.apps.googleusercontent.com",
     clientSecret:"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",
-    callbackURL: "https://cybros-backend.herokuapp.com/auth/google/callback",
+    callbackURL: "https://cybros-backend.herokuapp.com",
     passReqToCallback   : true
 },
 function(request:any, accessToken:any, refreshToken:any, profile:any, done:any) {
