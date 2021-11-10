de3fccea2e0e28ab0d258b07f17c8ec30fedf3c7
shivral somani <shivral312002@gmail.com>
2021-11-10 16:41:48 +0530

    ok

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import ormConfig from "./orm.config"
 import { ApolloServer } from "apollo-server-express"
 import { buildSchema } from "type-graphql"
 import { UserResolver } from "./resolvers/user"
+import session from "express-session"
+// import passport from "passport"
+const passport = require('passport');
+require('./passport');
 const main = async () => {
     const conn = await createConnection(ormConfig)
     conn.runMigrations()
@@ -16,8 +20,37 @@ const main = async () => {
     })
     await apolloServer.start()
     apolloServer.applyMiddleware({ app })
-    app.listen(4000, () => {
+    app.listen(3000, () => {
         console.log("server is running on port 4000")
+    })
+    app.use(session({secret:"lol",resave: false,
+      saveUninitialized: false,}))
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.get('/google',
+    passport.authenticate('google', {
+            scope:
+                ['email', 'profile']
+        }
+    ));
+
+    app.get("/failed", (req, res) => {
+    res.send("Failed")
+})
+app.get("/success", (req, res) => {
+    res.send(`Welcome ${req.user.email}`)
+})
+
+
+    app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failed',
+        
+    }),
+    function (req, res) {
+        console.log("works ?")
+        // res.redirect('/success')
+
     })
 }
 main()
