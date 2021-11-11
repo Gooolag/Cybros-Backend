166ad8e4e06b7bbe7a7c1757ddf18770ca45ebda
dreadarceus <dreadarceus@gmail.com>
2021-11-11 12:17:58 +0530

    res

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,5 @@
 import { Field, ObjectType } from "type-graphql";
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity } from "typeorm";
 @ObjectType()
 @Entity()
 export class User extends BaseEntity {
